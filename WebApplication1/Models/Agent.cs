efd34980cfcd9111eda05a27e35827479bf1bcf1
Matthew Noonan <mnoonan@msn.com>
2015-06-25 21:18:52 -0400

    Update to better IP masking

--- WebApplication1/Models/Agent.cs
+++ WebApplication1/Models/Agent.cs
@@ -13,5 +13,18 @@ namespace WebApplication1.Models
         public string Referrer { get; set; }
         public string PageType { get; set; }
         public DateTime Timestamp { get; set; }
+        public string IpAddressMasked { 
+            get
+            {
+                if (string.IsNullOrWhitespace(this.IpAddress) || this.IpAddress.Contains(":"))
+                {
+                    return this.IpAddress;
+                }
+                else
+                {
+                    return "xxx.xxx." + string.Join(".", this.IpAddress.Split('.').Skip(2).Take(2).ToArray());
+                }
+            } 
+            }
     }
 }
