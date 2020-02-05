0af2ba1ed45876ab475228f7805ca46b16d8065e
Matthew Noonan <mnoonan@msn.com>
2020-02-05 14:40:38 -0500

    added App Insights telemetry

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -26,6 +26,7 @@ namespace WebApplication2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
